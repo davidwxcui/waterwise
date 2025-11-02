bfdd42f5f5e80502f7aa583c2d0f04e24bdffb68
David Cui <davidcui6688@gmail.com>
2025-11-02 00:43:25 -0700

    initial files setup

---
+++ waterwise/app/src/main/java/com/davidwxcui/waterwise/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.davidwxcui.waterwise
+
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.davidwxcui.waterwise.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile
+            )
+        )
+        supportActionBar?.hide()//hide top bar
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        binding.fabAdd.setOnClickListener {
+            // Add your action here
+            Toast.makeText(this, "Add button clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
