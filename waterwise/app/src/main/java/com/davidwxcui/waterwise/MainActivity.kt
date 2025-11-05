3f21f8ee0b4e8f594c5b1befe8826650e53274ef
David Cui <davidcui6688@gmail.com>
2025-11-04 23:16:38 -0800

    fixed home page

--- waterwise/app/src/main/java/com/davidwxcui/waterwise/MainActivity.kt
+++ waterwise/app/src/main/java/com/davidwxcui/waterwise/MainActivity.kt
@@ -2,14 +2,13 @@ package com.davidwxcui.waterwise
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.davidwxcui.waterwise.databinding.ActivityMainBinding
-import com.davidwxcui.waterwise.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var currentDest: Int = R.id.navigation_home
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,45 +18,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Initial fragment
-        if (savedInstanceState == null) {
-            openHome()
-        }
+        // Get NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        // Bottom navigation
-        binding.navView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    if (currentDest != R.id.navigation_home) openHome()
-                    currentDest = R.id.navigation_home
-                    true
-                }
-                R.id.navigation_dashboard,
-                R.id.navigation_notifications,
-                R.id.navigation_profile -> {
-                    // Placeholder for other tabs
-                    currentDest = item.itemId
-                    true
-                }
-                else -> false
-            }
-        }
+        // Connect BottomNavigationView to NavController
+        binding.navView.setupWithNavController(navController)
 
-        // FAB click
+        // FAB action: only if current fragment is HomeFragment
         binding.fabAdd.setOnClickListener {
-            val f = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
-            if (f is HomeFragment) {
-                f.showFabQuickAdd()
+            val currentFragment = navHostFragment.childFragmentManager.fragments.firstOrNull()
+            if (currentFragment is com.davidwxcui.waterwise.ui.home.HomeFragment) {
+                currentFragment.showFabQuickAdd()
             }
-        }
-
-        // Set default selected tab
-        binding.navView.selectedItemId = R.id.navigation_home
-    }
-
-    private fun openHome() {
-        supportFragmentManager.commit {
-            replace(R.id.nav_host_fragment_activity_main, HomeFragment())
         }
     }
 }
